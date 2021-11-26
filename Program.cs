ab67a3c8fc22de28df0a9c1a673021b4895f1be7
Alemascarello <ultimoemailquefaco@gmail.com>
2021-11-26 19:33:40 +0000

    Tarefa 1

--- Program.cs
+++ Program.cs
@@ -1,36 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dotnetcore
 {
     class Program
     {
-        // Exercício 1:
-        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
-        // - Imprimir no console para cada aluno
-        //      - Nome do aluno
-        //      - Caso média maior ou igual 7:
-        //          - Passou no ano letivo
-        //          - Não passou no ano letivo
-        // Requisítos:
-        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
-        // - Utilizar For ou Foreach
 
-
-        // Exercício 2:
-        // - Fazer um programa que cadastre pessoas, contendo nome e idade;
-        // - Imprimir a media de idade das pessoas
-        // - Imprimir o nome e idade da pessoa mais velha
-        // - Imprimir o nome e idade da pessoa mais nova
-        // - Imprimir a quantidade de pessoas com maior idade
-        // Requisítos:
-        // - Utilizar List (Avg, Max, Min, Where)
-        // - Utilizar Classe Pessoa
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando Programa");
+            var xNotasZuqui = new List<float> { 7, 6, 5, 3 };
+            var xNotasBronza = new List<float> { 8, 9, 10, 8 };
 
-            var xNotasZuqui = new int[] { 7, 6, 5, 3 };
-            var xNotasBronza = new int[] { 8, 9, 10, 8 };
+            Console.WriteLine("Média Zuqui " + CalcularMedia(xNotasZuqui));
+            Console.WriteLine("Média Bronza " + CalcularMedia(xNotasBronza));
         }
+
+        public static float CalcularMedia(List<float> Notas)
+        {
+
+            float media = 0;
+
+            for (int i = 0; i < Notas.Count; i++)
+            {
+                media = media + Notas[i];
+            }
+            return media / Notas.Count;
+
+        }
+
     }
 }
